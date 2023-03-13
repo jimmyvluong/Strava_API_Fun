e05835d2aa76815a0fb091f8e1499c33d30120c8
Jimmy Luong <60137075+jimmyvluong@users.noreply.github.com>
2023-03-13 12:03:18 -0700

    remove pandos.io.json import

--- layout/layout.py
+++ layout/layout.py
@@ -9,7 +9,7 @@ import datetime
 # import sys
 # import packages for data manipulation
 import pandas as pd
-from pandas.io.json import json_normalize
+#from pandas.io.json import json_normalize
 import numpy as np
 
 # import packages for requesting data from the API
