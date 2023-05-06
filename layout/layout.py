d860f0b13ecd6b45200fdb7422a0f01e7d251d36
Jimmy Luong <60137075+jimmyvluong@users.noreply.github.com>
2023-05-05 22:06:53 -0700

    more test

--- layout/layout.py
+++ layout/layout.py
@@ -175,11 +175,12 @@ fig0.update_layout(bargap = 0.8)
 #############################################################################################
 
 strava_layout = html.Div(
-    html.Iframe(src="nfs_map.html", style={"width": "1000px", "height": "700px"}),
+
     [
 
         # Set the new white-text image.
         html.Img(src=header_img_link, style={"width": "587px", "height": "391px"}),
+        html.Iframe(src="nfs_map.html", style={"width": "1000px", "height": "700px"}),
         html.H1("Strava API Project"),
         # Add the time last updated.
         html.H3("Last updated: " + time_updated_UTC.strftime("%B %d %Y at %H:%M UTC")),
